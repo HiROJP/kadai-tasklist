9735dac8cfbe73ed44ffbda7f0664d69e8aeb118
HiROJP <zeal1167@gmail.com>
2019-08-23 06:55:06 +0000

    add column update

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,7 @@ ActiveRecord::Schema.define(version: 2019_08_23_050233) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
-    t.text "status"
+    t.string "status"
   end
 
 end
