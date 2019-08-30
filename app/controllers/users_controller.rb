3dbb1b17706e44850f040bdd2238eec54def5081
HiROJP <zeal1167@gmail.com>
2019-08-30 08:53:56 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :ensure_correct_user, only: [:index, :show, :users]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -23,6 +24,12 @@ class UsersController < ApplicationController
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
+    end
+  end
+  
+  def ensure_correct_user
+    if @current_user.id !=  params[:id].to_i
+      redirect_to @current_user
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
   root to: 'tasks#index'
   
-  resources :tasks
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
+  
+  resources :tasks
   resources :users, only: [:index, :show, :new, :create]
-  resources :tasks, only: [:create, :destroy, :edit]
   
 end
